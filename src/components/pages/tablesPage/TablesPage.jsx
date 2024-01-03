7178c7d460280fde6a4dca24f57ebe1a71335908
LuisTali <taliercioluis1@gmail.com>
2024-01-03 16:32:09 -0300

    Now the tables are obtained from the database

--- src/components/pages/tablesPage/TablesPage.jsx
+++ src/components/pages/tablesPage/TablesPage.jsx
@@ -1,10 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './TablesPage.css';
 import { Table } from "../../common/table/Table";
+import { db } from "../../../firebaseConfig.jsx";
+import {doc,collection,documentId,getDocs,where,query, Firestore} from 'firebase/firestore';
 import kitchenFloor from '../../../assets/kitchenFloor.jpg';
 
 export const TablesPage = () =>{
-    const [tables,setTables] = useState([{id:1,order:[]},{id:2,order:[]},{id:3,order:[]},{id:4,order:[]},{id:5,order:[]}]);
+    
+    //const [tables,setTables] = useState([{id:1,order:[]},{id:2,order:[]},{id:3,order:[]},{id:4,order:[]},{id:5,order:[]}]);
+    const [tables,setTables] = useState([]);
+    const [loading,setLoading] = useState(true);
+    
+    useEffect(()=>{
+        let tablesDb
+        getDocs(collection(db,"Tables"))
+        .then((res)=>{
+            //setLoading(false);
+            tablesDb = res.docs.map((table)=>{
+                return {
+                    ...table.data()
+                }
+            })
+            setTables(tablesDb);
+        })  
+        .catch((err) => console.log(err));  
+    },[])
+
+    if(loading) return <div className="tablesPage loading" style={{backgroundImage:`url(${kitchenFloor})`}}>
+        <h2>Loading...</h2>
+    </div>
+    
 
     return <div className="tablesPage" style={{backgroundImage:`url(${kitchenFloor})`}}>
         <div className="tables">
