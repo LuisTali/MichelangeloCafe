7740193876a369ab034bea8fb1948b643f374971
Luis Taliercio <taliercioluis1@gmail.com>
2024-01-05 15:19:31 -0300

    You can choose between filter the orders per parameter and if you want in ascendan or descendant order

--- src/components/pages/ordersPage/OrdersPage.jsx
+++ src/components/pages/ordersPage/OrdersPage.jsx
@@ -8,18 +8,19 @@ const OrdersPage = () =>{
     const [orders,setOrders] = useState([]);
     const [loading,setLoading] = useState(true);
     const [filter,setFilter] = useState("date");
+    const [order,setOrder] = useState("desc"); //Order Asc or Desc
 
     const handleClickOrder = () =>{
         let selectFilter = document.getElementById("selectFilter");
+        let orderFilter = document.getElementById("orderFilter");
         setFilter(selectFilter.value);
-        console.log(selectFilter.value);
+        setOrder(orderFilter.value);
     }
 
     useEffect(()=>{
         const ordersCollection = collection(db,"Orders");
-        getDocs(query(ordersCollection,orderBy(filter,"desc")))
+        getDocs(query(ordersCollection,orderBy(filter,order)))
         .then((res)=>{
-            console.log(res);
             let ordersDb = res.docs.map((item) =>{ 
                 return {
                     ...item.data(),
@@ -30,16 +31,28 @@ const OrdersPage = () =>{
             setLoading(false);
         })
         .catch((error) => console.log(error))
-    },[filter]);
+    },[order]);
 
     if(loading != true){
         return <div className="ordersPage">
-            <select id="selectFilter">
-                <option>client</option>
-                <option selected>date</option>
-                <option>totalPrice</option>
-            </select>
-            <button onClick={()=>handleClickOrder()}>ORDENAR</button>
+            <div className="filterOrderDiv">
+                <div className="selectGroup">
+                    <label>Ordenar por:</label>
+                    <select id="selectFilter">
+                        <option value="client">Cliente</option>
+                        <option defaultValue selected value="date">Fecha</option>
+                        <option value="totalPrice">Precio</option>
+                    </select>
+                </div>
+                <div className="selectGroup">
+                    <label>Orden:</label>
+                    <select id="orderFilter">
+                        <option value="asc">Ascendente</option>
+                        <option defaultValue selected value="desc">Descendente</option>
+                    </select>
+                </div>
+                <button onClick={()=>handleClickOrder()}>ORDENAR</button>
+            </div>
             <div className="orders">
                 {orders.map((order,index)=>{
                     return <div className="order" key={index}>
